a7b6448b914408695a0db4717a427cedca64d4fa
Rahmatuldani <danirahmatul@gmail.com>
2024-04-26 08:18:55 +0700

    first commit

---
+++ src/services/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.contact-manager.project.skyshi.io";
+
+export async function getAllContactsData() {
+  return await axios.get(`${BASE_URL}/contacts`).catch((error) => {
+    return error;
+  });
+}
+
+export async function addNewContact(payload) {
+  return await axios.post(`${BASE_URL}/contacts`, payload).catch((error) => {
+    return error;
+  });
+}
+
+// TODO:
+// 1. Buat sebuah fungsi untuk menghapus data kontak dengan mengirimkan id sebagai params dari kontak yang akan dihapus ke API menggunakan endpoint /contacts, axios method yang digunakan adalah delete
+// 2. Buat sebuah fungsi untuk mengubah data kontak dengan mengirimkan id sebagai params dari kontak yang akan diubah beserta dengan data perubahan terbaru dari kontak tersebut ke API menggunakan endpoint /contacts, axios method yang digunakan adalah put
